5c44040332f3103ff5478d0b0750ceed4d7ee81b
iankurao <kuraoian@gmail.com>
2019-07-07 04:47:26 +0300

    connect to postgres

--- Hood/settings.py
+++ Hood/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'Hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'ian',
+    'PASSWORD':'vionashina',
     }
 }
+
 
 
 # Password validation
