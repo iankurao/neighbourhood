be09e28e251ba7736612505a7da1d87cd479420d
iankurao <kuraoian@gmail.com>
2019-07-07 04:59:02 +0300

    update our models

--- hood_app/models.py
+++ hood_app/models.py
@@ -9,7 +9,23 @@ class Neighbourhood(models.Model):
     population=models.IntegerField()
     image = models.ImageField(upload_to = 'images/')
 
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+
 class Profile(models.Model):
     image=models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio=models.CharField(max_length=300)
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
