1012fc92f593b51444ac3376a22e51e230dbee3f
iankurao <kuraoian@gmail.com>
2019-07-07 04:54:50 +0300

    create a profile test case and run unit tests

--- hood_app/tests.py
+++ hood_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from . models import *
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+
+        self.new_profile=Profile(bio='i love basketball')
+    # test for instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    # testing the save mothod
+    def test_save_profile(self):
+        self.new_profile.create_profile()
+        profile=Profile.objects.all()
+        self.assertTrue(len(profile)>0)
