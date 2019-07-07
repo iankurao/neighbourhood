5f07a0102ef62cc2f015ae8f98cc44279eb81f7b
iankurao <kuraoian@gmail.com>
2019-07-07 04:35:20 +0300

    create a view function for the home page

--- hood_app/views.py
+++ hood_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views import generic
+
 
 # Create your views here.
+def home(request):
+    neighbourhoods = Neighbourhood.objects.all()
+    return render(request, 'index.html',{"neighbourhoods":neighbourhoods,})
