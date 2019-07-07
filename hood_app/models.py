69280b4191a792540652cc3133b1811cc0a9ed36
iankurao <kuraoian@gmail.com>
2019-07-07 04:53:00 +0300

    create a profile and neighbourhood model and run migrations

--- hood_app/models.py
+++ hood_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=60)
+    location=models.CharField(max_length=60)
+    population=models.IntegerField()
+    image = models.ImageField(upload_to = 'images/')
+
+class Profile(models.Model):
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio=models.CharField(max_length=300)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
